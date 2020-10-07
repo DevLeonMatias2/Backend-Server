41252d7c091f963e3bcc6e12f0cfe6476b131662
rleon <soluciondigital15@farmaciasperuanas.pe>
2020-10-07 18:46:49 -0500

    Fin seccion 10 - Auth

--- index.js
+++ index.js
@@ -15,17 +15,18 @@ const {dbConnection} = require('./database/config');
  //Configurar CORS
  app.use(cors());
 
+ //Lectura y parseo del body
+app.use( express.json());
+
  //Base de datos
 
  dbConnection();
 
- //Rutas
- app.get('/',(req,res)=>{
-     res.json({
-         ok:true,
-         msg:'Hola Mundo'
-     });
- });
+ // //Rutas
+
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
+
 
 
  app.listen(process.env.PORT,()=>{
---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+
+    /*
+    Path:/api/login
+     */
+
+
+
+
+    const { Router } = require('express');
+   const {login}= require('../controllers/auth');
+   const {check} = require( "express-validator");
+    const {validarCampos} = require( "../middlewares/validar-campos");
+
+const router = Router();
+
+    router.post('/',
+        [
+
+           check('password','La contraseña es obligatoria').not().isEmpty(),
+          check('email','El correo es obligatorio').isEmail(),
+         validarCampos
+        ],
+  login
+    );
+
+
+
+
+
+
+   module.exports= router;
